81160c419b072210a284bdfe7ca2ea100b7715ff
liguangzheng998@hotmail.com <liguangzheng998@hotmail.com>
2023-11-12 05:30:28 +0000

    worked on query and visualization

--- mylib/query.py
+++ mylib/query.py
@@ -2,6 +2,7 @@
 
 from pyspark.sql import SparkSession
 import matplotlib.pyplot as plt
+import numpy as np
 
 def query():
     spark = SparkSession.builder.appName("Query").getOrCreate()
@@ -16,17 +17,38 @@ def query():
 def viz():
     ## group by gender, calculate survival rate
     query_ = query().toPandas()
-    print(query_)
+    # print(query_)
     plt.bar(query_.Sex, query_.Survival)
     plt.title("Survival Rate for Titanic Passengers by Gender")
     plt.xlabel("Gender")
     plt.ylabel("Survival Rate")
     plt.show()
 
+    ## group by gender, survived
+    spark = SparkSession.builder.appName("Query").getOrCreate()
+    query_2 = (
+        "SELECT Sex, Survived, COUNT(*) as Count "
+        "FROM titanic_delta "
+        "GROUP BY Sex, Survived "
+    )
+    query2 = spark.sql(query_2).toPandas()
+    sur = query2[query2["Survived"] == 1].Count.tolist()
+    not_sur = query2[query2["Survived"] == 0].Count.tolist()
+    ## making plot
+    width = 0.35
+    labels = ["Female", "Male"]
+    x = np.arange(2)
+    fig, ax = plt.subplots()
+    bars1 = ax.bar(x - width / 2, not_sur, width, label="Not Survived")
+    bars2 = ax.bar(x + width / 2, sur, width, label="Survived")
 
+    ax.set_ylabel("Count")
+    ax.set_title("Survival Count by Gender")
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend()
 
-    ## group by PClass, calculate survival rate
-    pass
+    plt.show()
 
 if __name__ == "__main__":
     # print(type(query()))
